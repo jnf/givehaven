88e0777b1004e9867614e31ae847c778fc1d00d0
Brenna <brennaleker@gmail.com>
2015-10-21 14:29:33 -0700

    updated account so that only applicable info. is displayed

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,2 +1,28 @@
 class VolunteersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    locate_volunteer
+  end
+
+  def update
+    locate_volunteer
+    @volunteer.update(volunteer_params[:volunteer])
+    redirect_to account_path(@current_user.username)
+  end
+
+  private
+
+  def locate_volunteer
+    @volunteer = Volunteer.find_by(user_id: @current_user.id)
+  end
+
+  def volunteer_params
+    params.permit(volunteer: [:role, :bio, :approval_status])
+  end
+
 end
