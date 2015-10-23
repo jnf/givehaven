4be69e37f01b4d89e4c6907b42bda57851baeaeb
Brenna <brennaleker@gmail.com>
2015-10-23 16:16:00 -0700

    stripe payment working with test account

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => 'pk_test_Msjk9b3HH1YxTRRiE7SSKWbu',
+  :secret_key      => 'sk_test_d97V7Q0cwaVvcxHkIsfpYSTJ'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
