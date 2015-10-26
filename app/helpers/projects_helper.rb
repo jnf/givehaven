c23eb64cf6d40a53ad448dc482d444f5db0f54ae
Brenna <brennaleker@gmail.com>
2015-10-25 22:44:10 -0700

    added project view helper for percentages

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def percent_complete(project)
+    (project.total_donated/project.total_requested*100).ceil
+  end
+
+  def percent_remaining(project)
+    100 - percent_complete(project)
+  end
 end
