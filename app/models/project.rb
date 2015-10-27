a1d0c4ef4ad16e0686ade139488749a71b2de74d
Brenna <brennaleker@gmail.com>
2015-10-27 12:51:59 -0700

    fixed seed problem

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,18 +8,8 @@ class Project < ActiveRecord::Base
 
   validates :description, length: { maximum: 250 }
   validates :title, length: { maximum: 60 }
-  validates :project_details, :organization_details, length: { minimum: 1025 }
 
   #---------------uploaders-------------------------
   mount_uploader :image, ImageUploader
-
-  #---------------SCOPE-----------------------------
-  # scope :ordered_by_create, -> { order('projects.created_at') }
-  # scope :ordered_by_expire, -> { order('projects.expires_on') }
-  # scope :active_projects, -> { where(approval_status: 'approved') AND where(funding_status: 'not funded') }
-
-  def active_projects
-    Project.where('approval_status = ? AND funding_status = ?', 'approved', 'not funded')
-  end
 
 end
