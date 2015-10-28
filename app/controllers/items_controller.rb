482561495e9da091863f9388b0500f8ff684cd75
Brenna <brennaleker@gmail.com>
2015-10-28 09:46:26 -0700

    WIP- amazon product api

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :logged_in
+  req = Vacuum.new
+
+  req.configure key: ENV["AWS_PRODUCT_KEY"],
+                secret: ENV["AWS_SECRET_KEY"],
+                tag:    'givheaven'
+
 end
