a5740a3547447a08969efeb95592de5261a187b8
jnf <jeremy.flores@gmail.com>
2015-10-28 16:19:22 -0700

    tweak configs for my setup.

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = 'givehaven'
+  config.aws_bucket = 'givehaven-jnf'
   config.aws_acl    = 'public-read'
   config.aws_credentials = {
     :access_key_id      => ENV["AWS_S3_ACCESS_KEY"],
